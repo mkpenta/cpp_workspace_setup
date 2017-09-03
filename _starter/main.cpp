3f86e610eef97cfd8ce47c18a7f95125d50ae60b
Michael Penta <mpenta@necc.mass.edu>
2017-09-03 00:46:51 +0000

    directory fix

---
+++ _starter/main.cpp
@@ -0,0 +1,14 @@
+/*
+Michael Penta
+Project/Assignemnt/Lab example
+This pogram prompts a user and prints a result
+*/
+#include <iostream>
+
+int main()
+{
+    int n = 0;
+    std::cout << "enter a number"<< std::endl;
+    std::cin >> n;
+    std::cout << n  << std::endl;
+}
